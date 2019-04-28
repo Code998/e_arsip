4ae904402cc2144cc75b052cb83d38e20921c83f
Banana <lawneart675@gmail.com>
2019-04-28 20:40:46 +0800

    adding templates for outmail

--- assets/db/e_arsip.sql
+++ assets/db/e_arsip.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 24, 2019 at 02:38 AM
+-- Generation Time: Apr 28, 2019 at 02:40 PM
 -- Server version: 5.6.26
 -- PHP Version: 5.5.28
 
@@ -47,7 +47,7 @@ INSERT INTO `admin` (`id`, `username`, `password`) VALUES
 
 CREATE TABLE IF NOT EXISTS `arsip_surat` (
   `jenis` varchar(25) NOT NULL,
-  `no_surat` varchar(30) NOT NULL,
+  `no_surat` int(11) NOT NULL,
   `nik` varchar(16) DEFAULT NULL,
   `dari_kpd` varchar(50) NOT NULL,
   `indeks` varchar(30) DEFAULT NULL,
@@ -58,9 +58,21 @@ CREATE TABLE IF NOT EXISTS `arsip_surat` (
   `isi_ringkas` text,
   `laci` varchar(20) NOT NULL,
   `guide` varchar(30) NOT NULL,
+  `nama_pe` varchar(50) NOT NULL,
   `file` varchar(255) DEFAULT NULL,
   `admin` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `arsip_surat`
+--
+
+INSERT INTO `arsip_surat` (`jenis`, `no_surat`, `nik`, `dari_kpd`, `indeks`, `tanggal_surat`, `tanggal_input`, `alamat`, `perihal`, `isi_ringkas`, `laci`, `guide`, `nama_pe`, `file`, `admin`) VALUES
+('Surat Keluar', 2, '1234567891011121', 'James Albus', NULL, NULL, '2019-04-24 09:14:30', NULL, 'Surat Pengantar KTP', NULL, 'Surat Pengantar', 'KTP', 'Kiki Arnita', NULL, 'admin'),
+('Surat Masuk', 3, NULL, 'asdasdasd', NULL, '2019-04-01', '2019-04-25 02:14:47', NULL, 'asdasd', 'asdasd', 'Surat Dinas', 'Tugas Dinas', '', 'Rasional Bentuk Akar.pdf', 'admin'),
+('Surat Keluar', 4, '1231231231231231', 'James Albas', NULL, NULL, '2019-04-27 04:37:20', NULL, 'Surat Pengantar KTP', NULL, 'Surat Pengantar', 'KTP', 'Kiki Arnita', NULL, 'admin'),
+('Surat Keluar', 5, '1231231231231231', 'James Albas', NULL, NULL, '2019-04-28 18:06:59', NULL, 'Surat Keterangan SKCK', NULL, 'Surat Keterangan', 'SKCK', 'Kiki Arnita', NULL, 'admin'),
+('Surat Keluar', 6, '1231231231231231', 'James Albas', NULL, NULL, '2019-04-28 18:07:20', NULL, 'Surat Keterangan Domisili', NULL, 'Surat Keterangan', 'Domisili', 'Kiki Arnita', NULL, 'admin');
 
 -- --------------------------------------------------------
 
@@ -80,7 +92,7 @@ CREATE TABLE IF NOT EXISTS `pegawai` (
 --
 
 INSERT INTO `pegawai` (`nama`, `alamat`, `nip`, `jabatan`) VALUES
-('Kiki Arnita', 'Jalan Street Nomor 1', '1700200', 'Kepala Desa');
+('Kiki Arnita', 'Jalan Street Nomor 1', '1700201', 'Kepala Desa');
 
 -- --------------------------------------------------------
 
@@ -109,6 +121,7 @@ CREATE TABLE IF NOT EXISTS `penduduk` (
 --
 
 INSERT INTO `penduduk` (`nik`, `no_kk`, `nama`, `tempat_lahir`, `tanggal_lahir`, `usia`, `jenis_kelamin`, `status`, `alamat`, `agama`, `pekerjaan`, `kewarnegaraan`, `tanggal_buat`) VALUES
+('1231231231231231', '1111111111111112', 'James Albas', 'Venture Citadel', '1997-03-01', 40, 'L', 'Single', 'Venture Citadel Boulevard Number 13', 'Atheist', 'Mage', 'Do not have', '2019-04-26'),
 ('1234567891011121', '1000000000000000', 'James Albus', 'Malang', '1911-04-13', 100, 'L', 'Single', 'Jalan Ikan Piranha Kanan Nomor 12', 'Atheist', 'Penyihir', 'Indonesia', '2019-04-13');
 
 --
@@ -139,6 +152,15 @@ ALTER TABLE `pegawai`
 ALTER TABLE `penduduk`
   ADD PRIMARY KEY (`nik`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `arsip_surat`
+--
+ALTER TABLE `arsip_surat`
+  MODIFY `no_surat` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
